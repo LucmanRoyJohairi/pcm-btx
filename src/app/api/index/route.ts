4f0f39f84f977c92624d898c57eb5abcfc48e788
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2024-11-14 13:32:42 +0800

    initial commit

---
+++ src/app/api/index/route.ts
@@ -0,0 +1,7 @@
+// app/api/index/route.ts
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    console.log('You have successfully connected to Bitrix API');
+    return NextResponse.json({ message: 'Welcome to the Bitrix Auth API' });
+}
