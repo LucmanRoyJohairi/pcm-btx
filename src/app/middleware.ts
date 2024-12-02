f3d1aa1df92af50d2c387f95d2ec7e59544cfd74
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2024-12-02 13:51:31 +0800

    chore: updated middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -3,16 +3,17 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.get('auth_token')?.value;
+    console.log("🚀 ~ middleware ~ token:", token)
 
     // Define the paths that require authentication
     const protectedPaths = ['/'];
 
-    if (protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))) {
+    // if (protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))) {
     if (!token) {
         const loginUrl = new URL('/login', request.url);
         return NextResponse.redirect(loginUrl);
     }
-    }
+    // }
 
     return NextResponse.next();
 }
