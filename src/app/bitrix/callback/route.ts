e020032af0eb72352e993fe79b806089c972c086
Roy Johairi Lucman <lucman.royjohairi@gmail.com>
2024-12-10 14:59:31 +0800

    chore: updated configuration

--- src/app/bitrix/callback/route.ts
+++ src/app/bitrix/callback/route.ts
@@ -46,7 +46,7 @@ export async function GET(request: Request) {
             `https://pcm-btx-app.vercel.app/rest/user.current?auth=${tokenData.access_token}`
             );
             // const userInfo = await userInfoResponse.json();
-            console.log("User Response: 🚀🚀", userInfoResponse);
+            console.log("User Response: 🚀🚀", userInfoResponse.json());
 
             // Set token as an HTTP-only cookie in the response headers
             const origin = new URL(request.url).origin;
